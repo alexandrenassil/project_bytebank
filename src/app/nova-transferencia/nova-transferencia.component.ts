73304963b85c10e06b62d049f29348a0c8ae1bea
alexandrenassil <57010402+alexandrenassil@users.noreply.github.com>
2022-04-26 16:40:26 -0300

    Add files via upload

---
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -0,0 +1,39 @@
+import { TransferenciaService } from './../services/transferencia.service';
+import { HttpClient } from '@angular/common/http';
+import { Component, Output, EventEmitter } from "@angular/core";
+import { Transferencia } from 'src/models/transferencia.model';
+
+@Component({
+  selector: 'app-nova-transferencia',
+  templateUrl: './nova-transferencia.component.html',
+  styleUrls: ['./nova-transferencia.component.scss']
+})
+export class NovaTransferenciaComponent{
+
+  @Output() aoTransferir = new EventEmitter<any>();
+
+  valor: number;
+  destino: number;
+
+  constructor(private service: TransferenciaService){
+
+  }
+
+  transferir() {
+    console.log('Solicitado nova transferencia!');
+    const valorEmitir: Transferencia = {valor: this.valor, destino: this.destino};
+
+    this.service.adicionar(valorEmitir).subscribe(
+      (resultado) => {
+        console.log(resultado);
+        this.limparCampos();
+      }, (error) => console.error(error)
+
+    )
+  }
+
+  limparCampos(){
+    this.valor = 0;
+    this.destino = 0;
+  }
+}
